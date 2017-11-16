f6b3fdd3c0bd081d5513901095f2bd252bdd3fcc
Jesse Breneman <jmwashere@gmail.com>
2017-11-16 00:45:58 -0500

    set up emoji insertion

--- src/components/ChatBody/ChatInput.jsx
+++ src/components/ChatBody/ChatInput.jsx
@@ -35,19 +35,22 @@ export default class ChatInput extends Component {
 		this.setState({ picker: !this.state.picker });
 	}
 
+	_insertEmoji = (data) => {
+		const text = this.state.text.length > 0 ? `${this.state.text} ${data.shortname}` : data.shortname;
+		this.setState({ text });
+	};
+
 	render() {
 		return (
 			<div className="chat-input">
-				<input className="chat-input__input" type="text" placeholder="Chat..." onKeyPress={this._handleKeyPress} onInput={this._updateText} />
+				<input className="chat-input__input" type="text" placeholder="Chat..." value={this.state.text} onKeyPress={this._handleKeyPress} onInput={this._updateText} />
 				<button className="chat-input__emoji" onClick={this._togglePicker}>
 					<svg className="icon icon-smile" viewBox="0 0 32 32">
 						<path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM22 8c1.105 0 2 1.343 2 3s-0.895 3-2 3-2-1.343-2-3 0.895-3 2-3zM10 8c1.105 0 2 1.343 2 3s-0.895 3-2 3-2-1.343-2-3 0.895-3 2-3zM16 28c-5.215 0-9.544-4.371-10-9.947 2.93 1.691 6.377 2.658 10 2.658s7.070-0.963 10-2.654c-0.455 5.576-4.785 9.942-10 9.942z"></path>
 					</svg>
 				</button>
 				<div className={`chat-input__picker ${this.state.picker ? '' : 'hidden'}`}>
-					<EmojiPicker onChange={function(data){
-					  console.log("Emoji chosen", data);
-					}} />			
+					<EmojiPicker onChange={this._insertEmoji} />			
 				</div>
 			</div>
 		);
