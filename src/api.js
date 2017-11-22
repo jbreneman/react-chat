f9b8b2cdcfcfc476d46df47f7c44b4f9e7cb0ff5
Jesse Breneman <jmwashere@gmail.com>
2017-11-21 22:39:31 -0500

    add envs

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8900');
+const socket = openSocket(process.env.REACT_APP_SERVER_HOST);
 
 export const connect = (user) => {
 	socket.emit('userConnect', user);
